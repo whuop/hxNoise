87c7cb9d6006557cc481168c482b77416bd839c2
Kristian Brodal <kristianbrodal@gmail.com>
2016-03-25 17:45:49 +0100

    Working on improved perlin noise implementation. Not ready yet

--- samples/luxe/Perlin/src/Main.hx
+++ samples/luxe/Perlin/src/Main.hx
@@ -2,11 +2,11 @@
 import luxe.Input;
 import luxe.Color;
 
-import hxnoise.DiamondSquare;
+import hxnoise.Perlin;
 
 class Main extends luxe.Game {
 
-    private var m_diamondSquare : DiamondSquare;
+    private var m_perlin : Perlin;
 
     override function config(config:luxe.AppConfig) 
     {
@@ -17,20 +17,20 @@ class Main extends luxe.Game {
 
     override function ready() 
     {
-        var featureSize = 64;
-        var width = 256;
-        var height = 256;
-        var scale = 2.0;
-        m_diamondSquare = new DiamondSquare(width, height, featureSize, scale, randFunc);
+        m_perlin = new Perlin();
+        var width = 128;
+        var height = 128;
+        /*m_diamondSquare = new DiamondSquare(width, height, featureSize, scale, randFunc);
         m_diamondSquare.diamondSquare();
-
-        var pixelSize = 1.0;
+        */
+        var pixelSize = 2.0;
         for(x in 0...width)
         {
             for(y in 0...height)
             {
-                var c = m_diamondSquare.getValue(x,y);
+                var c = m_perlin.OctavePerlin(x + 0.1, y + 0.1, 0.1, 4, 1.0, 9.0, 2.0);
 
+                //trace(c);
                 Luxe.draw.box({    
                     x : x * pixelSize, y : y * pixelSize,
                     depth: -2,
