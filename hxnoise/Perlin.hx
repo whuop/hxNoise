16e44a4ef2dd51e1cfc00f90566851910e18dc11
Kristian Brodal <kristianbrodal@gmail.com>
2016-03-26 17:14:45 +0100

    First working version of Perlin Noise. Still some cleanup left.

--- hxnoise/Perlin.hx
+++ hxnoise/Perlin.hx
@@ -3,38 +3,43 @@ package hxnoise;
 class Perlin
 {
 	//	Hash lookup defined by Ken Perlin
-	private static var PERMUTATIONS = [151,160,137,91,90,15,                 
-    131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,    
-    190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
-    88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
-    77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-    102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-    135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
-    5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-    223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-    129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-    251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-    49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-    138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180];
+	private static var PERMUTATIONS : Array<Int> = [ 151,160,137,91,90,15,
+		131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,	
+		190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+		88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+		77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+		102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+		135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+		5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+		223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+		129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+		251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+		49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+		138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
+	];
 
-    private static var P : haxe.ds.Vector<Int>;
+    private static var P : Array<Int>;
+    public var repeat(default, default) : Int;
 
-	public function new() : Void
+
+	public function new(repeat : Int = -1) : Void
 	{
+		this.repeat = repeat;
 		//	Initialize permutation overflow vector
 		if (P == null)
-		{
-			P = new haxe.ds.Vector<Int>(512);
-			for(x in 0...512)
-			{
-				P[x] = PERMUTATIONS[x%256];
-			}
+		{				
+			P = [ for (x in 0 ... 512) PERMUTATIONS[x % 256] ];
 		}	
 	}
 
 	public function perlin(x : Float, y : Float, z : Float) : Float
 	{
-		// TODO: Repeat thing here
+		if (this.repeat > 0)
+		{
+			x = x % repeat;
+			y = y % repeat;
+			z = z % repeat;
+		}
 
 		var xi : Int = Math.floor(x) & 255;
 		var yi : Int = Math.floor(y) & 255;
@@ -103,39 +108,32 @@ class Perlin
 		return t * t * t * (t * (t * 6 - 15) + 10);
 	}
 
-	public function inc(num : Int) : Int
-	{
+	public function inc(num :Int) :Int {
 		num++;
-		// TODO: Repeat things here
+		if (repeat > 0) num %= repeat;
+		
 		return num;
 	}
 
-	public function grad(hash : Int, x : Float, y : Float, z : Float)
+	public static function grad(hash :Int, x :Float, y :Float, z :Float) :Float 
 	{
-		switch(hash & 0xF)
-		{
-			case 0x0: return x+y;
-			case 0x1: return -x + y;
-			case 0x2: return x - y;
-			case 0x3: return -x - y;
-			case 0x4: return x + z;
-			case 0x5: return -x + z;
-			case 0x6: return x - z;
-			case 0x7: return -x - z;
-			case 0x8: return y + z;
-			case 0x9: return -y + z;
-			case 0xA: return y - z;
-			case 0xB: return -y - z;
-			case 0xC: return y + x;
-			case 0xD: return -y + z;
-			case 0xE: return y - x;
-			case 0xF: return -y - z;
-		}
-		return 0;
+		var h :Int = hash & 15;
+		var u  :Float = h < 8 ? x : y;
+		
+		var v  :Float;
+		
+		if(h < 4)
+			v = y;
+		else if(h == 12 || h == 14)
+			v = x;
+		else 												
+			v = z;
+		
+		return ((h&1) == 0 ? u : -u)+((h&2) == 0 ? v : -v);
 	}
 
 	public function lerp(a : Float, b : Float, x : Float) 
 	{
-		return a + x * (b - a);
+		return a + (x * (b - a));
 	}
 }
--- samples/luxe/Perlin/src/Main.hx
+++ samples/luxe/Perlin/src/Main.hx
@@ -1,12 +1,15 @@
 
 import luxe.Input;
 import luxe.Color;
+import phoenix.Texture;
+import luxe.Sprite;
 
 import hxnoise.Perlin;
 
 class Main extends luxe.Game {
 
     private var m_perlin : Perlin;
+    private var m_texture : Texture;
 
     override function config(config:luxe.AppConfig) 
     {
@@ -17,18 +20,31 @@ class Main extends luxe.Game {
 
     override function ready() 
     {
+
         m_perlin = new Perlin();
         var width = 128;
         var height = 128;
         /*m_diamondSquare = new DiamondSquare(width, height, featureSize, scale, randFunc);
         m_diamondSquare.diamondSquare();
         */
+
+         m_texture = new Texture({
+            width : width,
+            height : height,
+            id : '0'
+        });
+
+        //var pixelBuf = new snow.api.buffers.Uint8Array();
+        //var pixels = m_texture.fetch(pixelBuf);
+
+
         var pixelSize = 2.0;
         for(x in 0...width)
         {
             for(y in 0...height)
             {
-                var c = m_perlin.OctavePerlin(x + 0.1, y + 0.1, 0.1, 4, 1.0, 9.0, 2.0);
+                var c = m_perlin.OctavePerlin(x / 8, y / 8, 0.1, 8, 0.5, 1.0, 10.0);
+                var color = new Color(c,c,c,1.0);
 
                 //trace(c);
                 Luxe.draw.box({    
@@ -36,10 +52,12 @@ class Main extends luxe.Game {
                     depth: -2,
                     w : 1 * pixelSize,
                     h : 1 * pixelSize,
-                    color : new Color(c,c,c)
+                    color : color
                 });
             }
         }
+
+        //var pixelBuf = new snow.api.buffers.Uint8Array(null, )
         
     } //ready
 
